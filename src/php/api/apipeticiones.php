5128f3680647ceef8815d8246593c5258ba9868f
Adrian <adrianthelawx@gmail.com>
2022-06-09 17:27:13 -0600

    Cambios meneros en api peticiones

--- src/php/api/apipeticiones.php
+++ src/php/api/apipeticiones.php
@@ -7,18 +7,15 @@ class ApiPilotos
      {
           $piloto = new Querys();
           $pilotos = array();
-          // $pilotos["items"] = array();
 
           $res = $piloto->get_pilotos();
           while ($fila = $res->fetch_assoc()) {
                $items = array(
                     'id' => $fila['id_piloto'],
                     'nombre' => $fila['nombre'],
-                    'victorias' => $fila['victorias'],
                );
                array_push($pilotos, $items);
           }
-          // echo json_encode($pilotos);
           return $pilotos;
      }
      function getCircuitos(){
@@ -30,11 +27,6 @@ class ApiPilotos
                $items = array(
                     'id' => $fila['id_circuito'],
                     'nombre' => $fila['nombre'],
-                    'pais' => $fila['pais'],
-                    'longitud' => $fila['longitud'],
-                    'vueltas' => $fila['vueltas'],
-                    'distancia total' => $fila['distancia_total'],
-                    'record vuelta' => $fila['record_vuelta'],
                );
                array_push($circuitos, $items);
           }
@@ -49,13 +41,6 @@ class ApiPilotos
                $items = array(
                     'id' => $fila['id_equipo'],
                     'nombre' => $fila['nombre'],
-                    'pais' => $fila['pais'],
-                    'director deportivo' => $fila['director_deportivo'],
-                    'jefe tecnico' => $fila['jefe_tecnico'],
-                    'chasis actual' => $fila['chasis_actual'],
-                    'motor' => $fila['motor'],
-                    'debut' => $fila['debut'],
-                    'campeonatos' => $fila['campeonatos'],
                );
                array_push($equipos, $items);
           }
@@ -67,4 +52,5 @@ $test = new ApiPilotos();
 $arrayPilotos = $test->getPilotos();
 $arrayCircuitos = $test->getCircuitos();
 $arrayEquipos = $test->getEquipos();
+
 ?>
