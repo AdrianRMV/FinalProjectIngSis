4a681001353554404621840cba61160275d2534d
Adrian <adrianthelawx@gmail.com>
2022-06-09 16:45:52 -0600

    Social medias en header

--- src/php/templates/footerHTML.php
+++ src/php/templates/footerHTML.php
@@ -3,5 +3,8 @@
 <!-- Bootstrap script -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
+<!-- Font Awesome -->
+<script src="https://kit.fontawesome.com/e8d2b3888e.js" crossorigin="anonymous"></script>
+
 </body>
 </html>
--- src/php/templates/header.php
+++ src/php/templates/header.php
@@ -2,4 +2,12 @@
      <a href="#" class='logo-container'>
           <img src='http://localhost/src/images/f1logo.png' alt='f1 logo' class="img-fluid" width="50px" height="auto">
      </a>
+     <div class="social-medias container">
+          <a href="https://www.facebook.com/Formula1" target="_blank" title="Facebook F1">
+               <i class="fa-brands fa-facebook-square"></i>
+          </a>
+          <a href="https://twitter.com/F1?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" target="_blank" title="Twitter F1">
+               <i class="fa-brands fa-twitter-square"></i>
+          </a>
+     </div>
 </header>
